adb1755373a7ab72973f0ea7330e1ae77164d97b
hinakkograshi <hinaf0224@gmail.com>
2024-03-21 06:29:30 +0900

    first comit

---
+++ PokeAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokeAPI
+//
+//  Created by Hina on 2024/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokeAPI/PokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPIApp.swift
+//  PokeAPI
+//
+//  Created by Hina on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
