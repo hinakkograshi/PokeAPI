b73ec92a9a43f48ac848b05eb52c2622e232da1b
uhooi <the_uhooi@yahoo.co.jp>
2024-03-27 22:49:29 +0900

    Update PokedexView.swift

--- PokeAPI/View/PokedexView.swift
+++ PokeAPI/View/PokedexView.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct PokedexView: View {
-    private let gridItem = [GridItem(.flexible()), GridItem(.flexible())]
-    //viewModelの参照し、Viewを描画
-    //この人たちを監視している　観測
+    private let gridItems: [GridItem] = [GridItem(.flexible()), GridItem(.flexible())]
+
+    // viewModelを参照し、Viewを描画
+    // この人たちを監視している　観測
     @ObservedObject var viewModel = PokemonViewModel()
+
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: gridItem, spacing: 20) {
-                    //発行した配列が更新されるたび、全ての変更を読み込み
+                LazyVGrid(columns: gridItems, spacing: 20) {
+                    // 発行した配列が更新されるたび、全ての変更を読み込み
                     ForEach(viewModel.pokemon) { pokemon in
                         PokemonCell(pokemon: pokemon, viewModel: viewModel)
                     }
